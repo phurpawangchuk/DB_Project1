9b63addcb930ccd88aac7a594e54cf0bee413b25
Marc Lennard Colina <marclennardcolina@Marcs-MacBook-Pro.local>
2024-05-08 16:15:21 -0500

    Second batch of Project answers

--- Q63-95.sql
+++ Q63-95.sql
@@ -233,14 +233,200 @@ WHERE
 
 -- 74) Retrieve the list of students who have submitted at least one assignment in all the courses
 -- they are enrolled in.
+SELECT
+    s.studentId,
+    s.studentName
+FROM
+    student s
+    JOIN student_course sc ON s.studentId = sc.studentId
+    LEFT JOIN (
+        SELECT
+            sa.studentId,
+            ca.courseId
+        FROM
+            student_assignment sa
+            JOIN course_assignment ca ON sa.assignmentId = ca.assignmentId
+        GROUP BY
+            sa.studentId,
+            ca.courseId
+    ) submitted ON submitted.studentId = s.studentId
+    AND submitted.courseId = sc.courseId
+GROUP BY
+    s.studentId,
+    s.studentName
+HAVING
+    COUNT(DISTINCT sc.courseId) = COUNT(DISTINCT submitted.courseId);
+
 -- 75) Retrieve the list of students who have not enrolled in any courses.
+SELECT
+    s.studentId,
+    s.studentName
+FROM
+    student s
+WHERE
+    s.studentId NOT IN (
+        SELECT
+            DISTINCT studentId
+        FROM
+            student_course
+    );
+
 -- 76) Retrieve the list of courses that have the highest number of enrolled students.
+WITH Num_Enrollees AS (
+    SELECT
+        courseId,
+        COUNT(*) AS Num_Enrollees_In_Course
+    FROM
+        student_course
+    GROUP BY
+        courseId
+)
+SELECT
+    ne.courseId,
+    c.courseName,
+    ne.Num_Enrollees_In_Course
+FROM
+    course c
+    INNER JOIN Num_Enrollees ne ON ne.courseId = c.courseId
+WHERE
+    ne.Num_Enrollees_In_Course = (
+        SELECT
+            MAX(Num_Enrollees_In_Course)
+        FROM
+            Num_Enrollees
+    );
+
 -- 77) Retrieve the list of assignments that have the lowest average grade.
+WITH Average_Grades AS (
+    SELECT
+        assignmentId,
+        AVG(assignment_numeric_grade) AS Average_Grade
+    FROM
+        student_assignment
+    GROUP BY
+        assignmentId
+)
+SELECT
+    ag.assignmentId,
+    a.assign_description,
+    ag.Average_Grade
+FROM
+    assignment a
+    INNER JOIN Average_Grades ag ON ag.assignmentId = a.assignmentId
+WHERE
+    ag.Average_Grade = (
+        SELECT
+            MIN(Average_Grade)
+        FROM
+            Average_Grades
+    );
+
 -- 78) Retrieve the list of students who have submitted all the assignments in a particular
--- course.
+-- course ("assuming course id is 1").
+WITH Student_Req_Assignments AS (
+    SELECT
+        sc.studentId,
+        COUNT(*) AS Num_Assignments_Required
+    FROM
+        student_course sc
+        INNER JOIN course_assignment ca ON ca.courseId = sc.courseId
+    WHERE
+        sc.courseId = 1
+    GROUP BY
+        sc.studentId
+),
+Student_Submission_Assignments AS (
+    SELECT
+        sa.studentId,
+        COUNT(*) AS Num_Assignments_Submitted
+    FROM
+        student_assignment sa
+        INNER JOIN course_assignment ca ON ca.assignmentId = sa.assignmentId
+    WHERE
+        ca.courseId = 1
+    GROUP BY
+        sa.studentId
+)
+SELECT
+    s.studentId,
+    s.studentName
+FROM
+    student s
+    INNER JOIN Student_Req_Assignments sqa ON sqa.studentId = s.studentId
+    INNER JOIN Student_Submission_Assignments ssa ON ssa.studentId = sqa.studentId
+WHERE
+    sqa.Num_Assignments_Required = ssa.Num_Assignments_Submitted;
+
 -- 79) Retrieve the list of courses where the average grade of all students is above 80.
+SELECT
+    c.courseId,
+    c.courseName,
+    AVG(stdcourse_numeric_grade) AS Average_Grade
+FROM
+    course c
+    INNER JOIN student_course sc ON sc.courseId = c.courseId
+GROUP BY
+    courseId
+HAVING
+    AVG(stdcourse_numeric_grade) > 80;
+
 -- 80) Retrieve the list of students who have the highest grade in each course.
+SELECT
+    c.courseName,
+    s.studentId,
+    s.studentName,
+    sc.stdcourse_numeric_grade AS Highest_Grade
+FROM
+    student s
+    INNER JOIN student_course sc ON sc.studentId = s.studentId
+    INNER JOIN course c ON c.courseId = sc.courseId
+WHERE
+    (sc.courseId, sc.stdcourse_numeric_grade) IN (
+        SELECT
+            courseId,
+            MAX(stdcourse_numeric_grade) AS Max_Grade_Per_Course
+        FROM
+            student_course
+        GROUP BY
+            courseId
+    )
+ORDER BY
+    c.courseName;
+
 -- 81) Retrieve the list of students who have submitted all the assignments on time.
+WITH Assignments_Timely_Submitted AS (
+    SELECT
+        studentId,
+        COUNT(*) AS Num_Assignments_On_Time
+    FROM
+        student_assignment sa
+        INNER JOIN assignment a ON a.assignmentId = sa.assignmentId
+    WHERE
+        sa.assignment_submission_date <= a.assign_due_date
+    GROUP BY
+        studentId
+),
+Assignments_Submitted AS (
+    SELECT
+        studentId,
+        COUNT(*) AS Num_Assignments_Submitted
+    FROM
+        student_assignment
+    GROUP BY
+        studentId
+)
+SELECT
+    s.studentId,
+    s.studentName,
+    ats.Num_Assignments_On_Time,
+    ass.Num_Assignments_Submitted
+FROM
+    student s
+    INNER JOIN Assignments_Timely_Submitted ats ON ats.studentId = s.studentId
+    INNER JOIN Assignments_Submitted ass ON ass.studentId = ats.studentId
+WHERE
+    ats.Num_Assignments_On_Time = ass.Num_Assignments_Submitted;
+
 -- 82) Retrieve the list of students who have submitted late submissions for any assignment.
 -- 83) Retrieve the list of courses that have the lowest average grade for a particular semester.
 -- 84) Retrieve the list of students who have not submitted any assignment for a particular
